89685acf8b57dc5a88bdf36d1885e8e56ce63f96
just-port <just-port@users.noreply.github.com>
2021-07-16 17:04:14 +0000

    deploy: 8b42dd38cdb833f5dd69a2ee70936695352c13e7

---
+++ precache-manifest.bd7066d2f395fa162dcb7923691cc852.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53de4c50dbe2e534070af0d20ecef006",
+    "url": "https://just-port.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "96446ac55b74617f8bd3",
+    "url": "https://just-port.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "b3a5ee391c91bc14cfd8",
+    "url": "https://just-port.github.io/task-management-frontend/static/js/2.7b204739.chunk.js"
+  },
+  {
+    "revision": "96446ac55b74617f8bd3",
+    "url": "https://just-port.github.io/task-management-frontend/static/js/main.dead136e.chunk.js"
+  },
+  {
+    "revision": "94cb4301686e66d518eb",
+    "url": "https://just-port.github.io/task-management-frontend/static/js/runtime~main.9796d6c7.js"
+  }
+]);
